79f5b36caebe8c95357c260cc93f93576a311cb0
Soham Sabale <sabale.soham@gmail.com>
2024-06-28 17:39:56 -0400

    next commit

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/')
+      .then(response => response.json())
+      .then(data => setMessage(data.message));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{message}</h1>
       </header>
     </div>
   );
