f96d7024cd021fde3039c8289219b655c9fb5cb5
Soham Sabale <sabale.soham@gmail.com>
2024-07-02 11:25:50 -0400

    updates

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -5,7 +5,7 @@ function App() {
 
   useEffect(() => {
     // fetch('http://localhost:8000/')
-    fetch('http://159.65.172.123:8000/')
+    fetch('http://142.93.67.138:8000/')
       .then(response => response.json())
       .then(data => setData(data))
       .catch(error => console.error('Error fetching data:', error));
@@ -14,7 +14,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>FastAPI + React App</h1>
+        <h1>FastAPI and React App</h1>
         {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : <p>Loading...</p>}
       </header>
     </div>
