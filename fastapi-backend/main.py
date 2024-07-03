56c5e664bb060a5e2f12d4f432d3033dee2f3fc1
Soham Sabale <sabale.soham@gmail.com>
2024-07-03 12:07:52 -0400

    updates

--- fastapi-backend/main.py
+++ fastapi-backend/main.py
@@ -13,5 +13,5 @@ app.add_middleware(
 
 @app.get("/")
 async def read_root():
-    return {"message": "Hello World - July 3rd  Deployment"}
+    return {"message": "Hello World- July 3rd  Deployment"}
 
